26f4193bfc5adf3a3b1fef3743827fb385b48df3
widnyana <wid@widnyana.web.id>
2026-08-14 18:49:34 +0700

    chore(invoice): remove dead DefaultTemplateFooter component
    Exported placeholder that rendered a literal '-' with zero importers repo-wide.

--- app/invoice/templates/default/components/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import { Text } from "@react-pdf/renderer";
-import { defaultTemplateStyles } from "../styles";
-
-export const DefaultTemplateFooter = () => {
-  return <Text style={defaultTemplateStyles.footer}>-</Text>;
-};
